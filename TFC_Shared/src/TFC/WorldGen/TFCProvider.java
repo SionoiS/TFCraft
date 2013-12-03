34fcac2de829fa7d3e742b6401e98668ddaf2f1b
SionoiS <sim.vivier@hotmail.com>
2013-12-03 17:18:59 -0500

    Player will now spawn at -17500

--- TFC_Shared/src/TFC/WorldGen/TFCProvider.java
+++ TFC_Shared/src/TFC/WorldGen/TFCProvider.java
@@ -118,7 +118,7 @@ public class TFCProvider extends WorldProvider
 		int var6 = 0;
 		int var7 = getAverageGroundLevel();
 		int var8 = 10000;
-		int startingZ = 3000 + var4.nextInt(12000);
+		int startingZ = 17500;
 
 		while(chunkcoordinates == null)
 		{
